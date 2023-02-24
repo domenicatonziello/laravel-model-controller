0d17090d0a83d98730b4b0fbdb28c1a7418669d2
domenicatonziello <domenica.tonziello10@gmail.com>
2023-02-24 17:47:02 +0100

    card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,24 @@
     @vite('resources/js/app.js')
 </head>
 <body>
-    <h1>Laravel Movies</h1>
+    <div class="container">
+        <h1 class="my-4 mx-0 text-center">Laravel Movies</h1>
+        <div class="row">
+            @forelse ($movies as $movie)
+                <div class="card text-bg-dark m-1" style="max-width: 17rem;">
+                    <div class="card-header text-danger">Film {{$movie->id}}</div>
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $movie->title }}</h5>
+                        <p class="card-text"><strong class="text-white-50"> Original Title: </strong> {{ $movie->original_title}}</p>
+                        <p class="card-text"><strong class="text-white-50"> Nationality: </strong> {{ $movie->nationality }}</p>
+                        <p class="card-text"><strong class="text-white-50"> Date: </strong>  {{ $movie->date }}</p>
+                        <p class="card-text"><strong class="text-white-50"> Vote: </strong>  {{ $movie->vote }}</p>
+                    </div>
+                </div>
+            @empty
+                <h3> Nessun Film disponibile </h3>   
+            @endforelse
+        </div>
+    </div>
 </body>
 </html>
